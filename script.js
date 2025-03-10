54a8c20b351dc6c148b92c865c18a9046d8b0e49
omrmhd5 <omrmhd54@gmail.com>
2025-03-10 23:39:40 +0200

    Changed Var Names

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        background: "#dfd6c7", // rgb(223, 214, 199)
-        content: "#f5efe0", // rgb(245, 239, 224)
-        text: "#976c60", // rgb(151, 108, 96)
+        bg_clr: "#dfd6c7", // rgb(223, 214, 199)
+        cn_clr: "#f5efe0", // rgb(245, 239, 224)
+        t_clr: "#976c60", // rgb(151, 108, 96)
       },
       fontFamily: {
         header: ["Lora", "serif"],
