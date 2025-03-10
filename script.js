b5ecef31d71fde1a216d37026e3d1c94b9b90d49
omrmhd5 <omrmhd54@gmail.com>
2025-03-10 23:21:28 +0200

    Added Tailwind

---
+++ script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        background: "#dfd6c7", // rgb(223, 214, 199)
+        content: "#f5efe0", // rgb(245, 239, 224)
+        text: "#976c60", // rgb(151, 108, 96)
+      },
+      fontFamily: {
+        header: ["Lora", "serif"],
+        paragraph: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+};
