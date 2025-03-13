058444eb1a3b52fb4d9b7da77a472482ae4e0a05
Rana Ali <Ranahatem.ab@gmail.com>
2025-03-13 20:28:46 +0200

    View all

--- src/ShopPage.jsx
+++ src/ShopPage.jsx
@@ -19,24 +19,28 @@ const productswomen = [
   { id: 12, name: "Boots", price: 350, image: "src/assets/img1.jpg" },
 ];
 
+const allProducts = [...productsmen, ...productswomen];
+
 export default function ShopPage() {
-  const [selectedProducts, setSelectedProducts] = useState([...productsmen, ...productswomen]);
+  const [selectedProducts, setSelectedProducts] = useState(allProducts);
 
   return (
     <div className="p-4 bg-bg_clr text-t_clr font-paragraph [&_h1]:font-header [&_h2]:font-header [&_h3]:font-header [&_h4]:font-header [&_h5]:font-header [&_h6]:font-header">
       <header className="flex justify-between items-center border-b pb-2 mb-4">
         <h1 className="text-xl font-bold font-header">Shop</h1>
-        <div>
-          <button className="mr-2 px-4 py-2 bg-bg_clr text-white" onClick={() => setSelectedProducts(productsmen)}>Men</button>
-          <button className="px-4 py-2 bg-bg_clr text-white" onClick={() => setSelectedProducts(productswomen)}>Women</button>
-        </div>
+        
       </header>
+      <div className="flex items-center justify-center ">
+          <button className="mr-2 px-4 py-2 bg-bg_clr text-t_clr" onClick={() => setSelectedProducts(productsmen)}>Men</button>
+          <button className="mr-2 px-4 py-2 bg-bg_clr text-t_clr" onClick={() => setSelectedProducts(productswomen)}>Women</button>
+          <button className="px-4 py-2 bg-bg_clr text-t_clr" onClick={() => setSelectedProducts(allProducts)}>View All</button>
+        </div>
       <div className="grid grid-cols-4 gap-4">
         {selectedProducts.map((product) => (
           <div key={product.id} className="text-center bg-cn_clr p-4 rounded">
             <img src={product.image} alt={product.name} className="w-full h-40 object-cover" />
             <h2 className="text-lg font-semibold mt-2 font-header">{product.name}</h2>
-            <p className="text-[--color-cn_cl]">${product.price}</p>
+            <p className="text-t_clr">${product.price}</p>
             <button className="mt-2 w-full bg-bg_clr text-white">Add to Cart</button>
           </div>
         ))}
