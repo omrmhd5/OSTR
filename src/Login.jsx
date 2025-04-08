03802af53a7951788782629e86de229de9467ef8
omrmhd5 <omrmhd54@gmail.com>
2025-04-08 19:57:28 +0200

    Updated Form Animations

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from "react-router";
+import { AnimatePresence, motion } from "framer-motion";
 
 const App = () => {
   const [activeTab, setActiveTab] = useState("SignIn");
@@ -36,7 +37,9 @@ const App = () => {
         ];
   });
 
-  localStorage.setItem("users", JSON.stringify(users));
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
 
   const handleSignInSubmit = (values, { setErrors }) => {
     const storedUsers = JSON.parse(localStorage.getItem("users"));
@@ -50,7 +53,6 @@ const App = () => {
       navigate("/");
     } else {
       setErrors({ password: "Incorrect email or password" });
-      console.log("Incorrect");
     }
   };
 
@@ -66,22 +68,17 @@ const App = () => {
       password: values.password,
     };
 
-    const updatedUsers = [...users, newUser];
-    setUsers(updatedUsers);
-    localStorage.setItem("users", JSON.stringify(updatedUsers));
-    console.log("🚀 ~ handleSignUpSubmit ~ users:", updatedUsers);
-    console.log("added");
+    setUsers((users) => [...users, newUser]);
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-100 to-purple-100 font-paragraph [&_h1]:font-header [&_h2]:font-header [&_h3]:font-header [&_h4]:font-header [&_h5]:font-header [&_h6]:font-header">
-      <div className="relative w-[800px] h-[500px] bg-white rounded-4xl shadow-2xl overflow-hidden">
+      <main className="relative w-[800px] h-[500px] bg-white rounded-4xl shadow-2xl overflow-hidden">
         {/* Beige Background with Animation */}
-        <div
+        <section
           className={`absolute top-0 h-full w-1/2 rounded-4xl bg-gradient-to-b from-bg_clr to-t_clr transition-all duration-1000 ease-in-out ${
             activeTab == "SignUp" ? "left-0" : "left-1/2"
-          }`}
-        >
+          }`}>
           <div className="flex flex-col items-center justify-center h-full text-white px-8">
             {activeTab == "SignUp" ? (
               <>
@@ -91,8 +88,7 @@ const App = () => {
                 </p>
                 <button
                   onClick={() => setActiveTab("SignIn")}
-                  className="cursor-pointer px-15 py-3 mt-8 font-bold border border-white rounded-xl hover:bg-bg_clr hover:text-t_clr transition"
-                >
+                  className="cursor-pointer px-15 py-3 mt-8 font-bold border border-white rounded-xl hover:bg-bg_clr hover:text-t_clr transition">
                   SIGN IN
                 </button>
               </>
@@ -105,172 +101,180 @@ const App = () => {
                 </p>
                 <button
                   onClick={() => setActiveTab("SignUp")}
-                  className="cursor-pointer px-15 py-3 mt-8 font-bold border border-white rounded-xl hover:bg-bg_clr hover:text-t_clr transition"
-                >
+                  className="cursor-pointer px-15 py-3 mt-8 font-bold border border-white rounded-xl hover:bg-bg_clr hover:text-t_clr transition">
                   SIGN UP
                 </button>
               </>
             )}
           </div>
-        </div>
+        </section>
 
-        {/* Sign In Form */}
-        {activeTab == "SignIn" && (
-          <div className="absolute top-0 left-0 h-full w-1/2 transition-all duration-1000 opacity-100">
-            <div className="flex flex-col items-center justify-center h-full px-8">
-              <h2 className="text-4xl font-bold mb-6">Sign In</h2>
-              <div className="flex space-x-4 mb-6">
-                <button className="cursor-pointer w-10 h-10 border rounded-xl flex items-center justify-center hover:bg-gray-100">
-                  <i className="fa-brands fa-google"></i>
-                </button>
-                <button className="cursor-pointer w-10 h-10 border rounded-xl flex items-center justify-center hover:bg-gray-100">
-                  <i className="fa-brands fa-facebook"></i>
-                </button>
+        <AnimatePresence mode="wait">
+          {/* Sign In Form */}
+          {activeTab == "SignIn" && (
+            <motion.section
+              key="signIn"
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -50 }}
+              transition={{ duration: 0.5 }}
+              className="absolute top-0 left-0 h-full w-1/2">
+              <div className="flex flex-col items-center justify-center h-full px-8">
+                <h2 className="text-4xl font-bold mb-6">Sign In</h2>
+                <div className="flex space-x-4 mb-6">
+                  <button className="cursor-pointer w-10 h-10 border rounded-xl flex items-center justify-center hover:bg-gray-100">
+                    <i className="fa-brands fa-google"></i>
+                  </button>
+                  <button className="cursor-pointer w-10 h-10 border rounded-xl flex items-center justify-center hover:bg-gray-100">
+                    <i className="fa-brands fa-facebook"></i>
+                  </button>
+                </div>
+                <p className="text-sm text-gray-500 mb-4">
+                  or use your email password
+                </p>
+
+                {/* Sign In Formik */}
+                <Formik
+                  initialValues={{ email: "", password: "" }}
+                  validationSchema={signInvalidationSchema}
+                  onSubmit={handleSignInSubmit}>
+                  {({ isSubmitting }) => (
+                    <Form className="flex flex-col w-full gap-2">
+                      <Field
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
+                      />
+                      <ErrorMessage name="email">
+                        {(msg) => (
+                          <div className="  font-semibold text-red-500 text-sm w-full ">
+                            ⚠️ {msg} !
+                          </div>
+                        )}
+                      </ErrorMessage>
+                      <Field
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
+                      />
+
+                      <ErrorMessage name="password">
+                        {(msg) => (
+                          <div className="font-semibold text-red-500 text-sm w-full ">
+                            ⚠️ {msg} !
+                          </div>
+                        )}
+                      </ErrorMessage>
+                      <div className="items-center flex flex-col ">
+                        <a
+                          href="#"
+                          className=" text-center text-sm text-gray-500 mb-4 hover:underline">
+                          FORGET YOUR PASSWORD?
+                        </a>
+                        <button
+                          type="submit"
+                          disabled={isSubmitting}
+                          className="cursor-pointer px-8 py-2 bg-t_clr text-white rounded-lg hover:bg-bg_clr hover:text-t_clr transition">
+                          SIGN IN
+                        </button>
+                      </div>
+                    </Form>
+                  )}
+                </Formik>
               </div>
-              <p className="text-sm text-gray-500 mb-4">
-                or use your email password
-              </p>
+            </motion.section>
+          )}
 
-              {/* Sign In Formik */}
-              <Formik
-                initialValues={{ email: "", password: "" }}
-                validationSchema={signInvalidationSchema}
-                onSubmit={handleSignInSubmit}
-              >
-                {({ isSubmitting }) => (
-                  <Form className="flex flex-col w-full gap-2">
-                    <Field
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
-                    />
-                    <ErrorMessage name="email">
-                      {(msg) => (
-                        <div className="  font-semibold text-red-500 text-sm w-full ">
-                          ⚠️ {msg} !
-                        </div>
-                      )}
-                    </ErrorMessage>
-                    <Field
-                      type="password"
-                      name="password"
-                      placeholder="Password"
-                      className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
-                    />
+          {/* Sign Up Form */}
+          {activeTab === "SignUp" && (
+            <motion.section
+              key="signUp"
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 50 }}
+              transition={{ duration: 0.5 }}
+              className="absolute top-0 left-1/2 h-full w-1/2">
+              <div className="flex flex-col items-center justify-center h-full px-8 mt-8">
+                <h2 className="text-3xl font-bold mb-6 ">Create Account</h2>
+                <div className="flex space-x-4 mb-6 font-bold">
+                  <button className="cursor-pointer w-10 h-10 border rounded-xl flex items-center justify-center hover:bg-gray-100">
+                    <i className="fa-brands fa-google"></i>
+                  </button>
+                  <button className="cursor-pointer w-10 h-10 border rounded-xl flex items-center justify-center hover:bg-gray-100">
+                    <i className="fa-brands fa-facebook"></i>
+                  </button>
+                </div>
+                <p className="text-sm text-gray-500 mb-2">
+                  or use your email for registration
+                </p>
 
-                    <ErrorMessage name="password">
-                      {(msg) => (
-                        <div className="font-semibold text-red-500 text-sm w-full ">
-                          ⚠️ {msg} !
-                        </div>
-                      )}
-                    </ErrorMessage>
-                    <div className="items-center flex flex-col ">
-                      <a
-                        href="#"
-                        className=" text-center text-sm text-gray-500 mb-4 hover:underline"
-                      >
-                        FORGET YOUR PASSWORD?
-                      </a>
-                      <button
-                        type="submit"
-                        disabled={isSubmitting}
-                        className="cursor-pointer px-8 py-2 bg-t_clr text-white rounded-lg hover:bg-bg_clr hover:text-t_clr transition"
-                      >
-                        SIGN IN
-                      </button>
-                    </div>
-                  </Form>
-                )}
-              </Formik>
-            </div>
-          </div>
-        )}
-
-        {/* Sign Up Form */}
-        {activeTab === "SignUp" && (
-          <div className="absolute top-0 left-1/2 h-full w-1/2 transition-all duration-1000 opacity-100">
-            <div className="flex flex-col items-center justify-center h-full px-8 mt-8">
-              <h2 className="text-3xl font-bold mb-6 ">Create Account</h2>
-              <div className="flex space-x-4 mb-6 font-bold">
-                <button className="cursor-pointer w-10 h-10 border rounded-xl flex items-center justify-center hover:bg-gray-100">
-                  <i className="fa-brands fa-google"></i>
-                </button>
-                <button className="cursor-pointer w-10 h-10 border rounded-xl flex items-center justify-center hover:bg-gray-100">
-                  <i className="fa-brands fa-facebook"></i>
-                </button>
+                <Formik
+                  initialValues={{
+                    name: "",
+                    email: "",
+                    password: "",
+                  }}
+                  validationSchema={signUpvalidationSchema}
+                  onSubmit={handleSignUpSubmit}>
+                  {({ isSubmitting }) => (
+                    <Form className="flex flex-col w-full gap-2">
+                      <Field
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        className="w-full p-2 px-4 border rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-500"
+                      />
+                      <ErrorMessage name="name">
+                        {(msg) => (
+                          <div className="  font-semibold text-red-500 text-sm w-full ">
+                            ⚠️ {msg} !
+                          </div>
+                        )}
+                      </ErrorMessage>
+                      <Field
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        className="w-full p-2 px-4 border rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-500"
+                      />
+                      <ErrorMessage name="email">
+                        {(msg) => (
+                          <div className="  font-semibold text-red-500 text-sm w-full ">
+                            ⚠️ {msg} !
+                          </div>
+                        )}
+                      </ErrorMessage>
+                      <Field
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        className="w-full p-2 px-4 border rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-500"
+                      />
+                      <ErrorMessage name="password">
+                        {(msg) => (
+                          <div className="  font-semibold text-red-500 text-sm w-full ">
+                            ⚠️ {msg} !
+                          </div>
+                        )}
+                      </ErrorMessage>
+                      <div className="flex justify-center">
+                        <button
+                          type="submit"
+                          disabled={isSubmitting}
+                          className="cursor-pointer px-8 py-2 bg-t_clr text-white rounded-lg hover:bg-bg_clr hover:text-t_clr transition">
+                          SIGN UP
+                        </button>
+                      </div>
+                    </Form>
+                  )}
+                </Formik>
               </div>
-              <p className="text-sm text-gray-500 mb-2">
-                or use your email for registration
-              </p>
-
-              <Formik
-                initialValues={{
-                  name: "",
-                  email: "",
-                  password: "",
-                }}
-                validationSchema={signUpvalidationSchema}
-                onSubmit={handleSignUpSubmit}
-              >
-                {({ isSubmitting }) => (
-                  <Form className="flex flex-col w-full gap-2">
-                    <Field
-                      type="text"
-                      name="name"
-                      placeholder="Name"
-                      className="w-full p-2 px-4 border rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-500"
-                    />
-                    <ErrorMessage name="name">
-                      {(msg) => (
-                        <div className="  font-semibold text-red-500 text-sm w-full ">
-                          ⚠️ {msg} !
-                        </div>
-                      )}
-                    </ErrorMessage>
-                    <Field
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      className="w-full p-2 px-4 border rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-500"
-                    />
-                    <ErrorMessage name="email">
-                      {(msg) => (
-                        <div className="  font-semibold text-red-500 text-sm w-full ">
-                          ⚠️ {msg} !
-                        </div>
-                      )}
-                    </ErrorMessage>
-                    <Field
-                      type="password"
-                      name="password"
-                      placeholder="Password"
-                      className="w-full p-2 px-4 border rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-500"
-                    />
-                    <ErrorMessage name="password">
-                      {(msg) => (
-                        <div className="  font-semibold text-red-500 text-sm w-full ">
-                          ⚠️ {msg} !
-                        </div>
-                      )}
-                    </ErrorMessage>
-                    <div className="flex justify-center">
-                      <button
-                        type="submit"
-                        disabled={isSubmitting}
-                        className="cursor-pointer px-8 py-2 bg-t_clr text-white rounded-lg hover:bg-bg_clr hover:text-t_clr transition"
-                      >
-                        SIGN UP
-                      </button>
-                    </div>
-                  </Form>
-                )}
-              </Formik>
-            </div>
-          </div>
-        )}
-      </div>
+            </motion.section>
+          )}
+        </AnimatePresence>
+      </main>
     </div>
   );
 };
