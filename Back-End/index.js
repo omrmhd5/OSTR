0e5a42ab20942994d7e310e0b781f9a6abafc8cd
omrmhd5 <omrmhd54@gmail.com>
2025-04-28 01:55:05 +0300

    Done Products, Categories, Still reviews and Get Categories By ID

--- Back-End/index.js
+++ Back-End/index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-// const taskRoutes = require('./routes/taskRoutes');
+const ProductRoutes = require("./Routes/ProductRoutes");
+const CategoryRoutes = require("./Routes/CategoryRoutes");
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-// app.use('/tasks', taskRoutes);
+app.use("/products", ProductRoutes);
+app.use("/category", CategoryRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
