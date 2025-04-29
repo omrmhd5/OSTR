ff2397e346c5daf3bf841be6c56b24bc5b125ba6
Rana Ali <Ranahatem.ab@gmail.com>
2025-04-29 16:58:03 +0300

    done with wishlist and cart but not style yours

--- Front-End/src/StyleYours.jsx
+++ Front-End/src/StyleYours.jsx
@@ -1,7 +1,7 @@
 import { SketchPicker } from "react-color";
 import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { useCart } from "./context/CartContext";
+
 
 import {
   Select,
@@ -19,7 +19,6 @@ export default function StyleYours() {
     b: "19",
     a: "1",
   });
-  const { addToCart, fetchCart } = useCart();
 
   const [color, setColor] = useState("#9B9B9B");
   const [textColor, setTextColor] = useState("#000000");
@@ -521,31 +520,7 @@ export default function StyleYours() {
         ? "bg-gray-600 cursor-not-allowed dark:bg-gray-400"
         : "bg-gray-800 text-white cursor-pointer hover:animate-bounce dark:bg-black"
     }`}
-    onClick={async () => {
-      // Loop over each size and add if quantity > 0
-      try {
-        const sizeEntries = Object.entries(quantities);
-        for (const [size, qty] of sizeEntries) {
-          if (qty > 0) {
-            await addToCart("custom-style-id", qty, {
-              size,
-              color,
-              text,
-              textColor,
-              fontSize,
-              fontFamily,
-              item,
-            });
-          }
-        }
-    
-        await fetchCart(); // update UI
-        handleMessage();
-      } catch (err) {
-        console.error("Failed to add custom product to cart", err);
-      }
-    }}
->    
+              onClick={handleMessage}>
               Add To Cart <i className="fas fa-shopping-cart"></i>
             </Button>
 
