e500090ca2d339a2b3a07a926fddecf799d087a0
omrmhd5 <omrmhd54@gmail.com>
2025-07-20 23:48:44 +0300

    Added Environment URLS

--- Front-End/src/lib/utils.js
+++ Front-End/src/lib/utils.js
@@ -5,4 +5,7 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-export const BASE_URL = "https://ostr.onrender.com";
+export const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_DEV_SERVER_URL
+    : import.meta.env.VITE_PROD_SERVER_URL;
